778e37a3340cf13d2afddac9dae44c50ded06570
League Student <league@iMac-4.attlocal.net>
2019-04-29 16:52:17 -0700

    IntroToArrayLists; GuestBook(IP)

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,12 +1,35 @@
 package intro_to_array_lists;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
-	// all the names added to the list. Format the list as follows:
-	// Guest #1: Bob Banders
-	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners
+	// Create a GUI with two buttons. One button reads "Add Name" and the other
+	// button reads "View Names".
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JButton b1 = new JButton();
+		JButton b2 = new JButton();
+
+		frame.setVisible(true);
+		frame.setTitle("Guest Book");
+		frame.add(panel);
+		panel.add(b1);
+		panel.add(b2);
+
+		b1.setText("Add Name");
+		b2.setText("View Names");
+		frame.pack();
+
+		// When the add name button is clicked, display an input dialog that asks the user to enter a name. 
+		
+		// Add that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays all the names added to the list. 
+		// Format the list as follows:
+		// Guest #1: Bob Banders
+		// Guest #2: Sandy Summers
+		// Guest #3: Greg Ganders
+		// Guest #4: Donny Doners
+	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,51 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList<String> weekDays = new ArrayList <String>();
 		//2. Add five Strings to your list
-		
+		weekDays.add("Monday");
+		weekDays.add("Tuesday");
+		weekDays.add("Wednesday");
+		weekDays.add("Thursday");
+		weekDays.add("Friday");
 		//3. Print all the Strings using a standard for-loop
+		for(int f = 0; f < weekDays.size(); f++) {
+			String w = weekDays.get(f);
+			System.out.println("String at element " + f + " " + w);
+			
+		}
+		
+		System.out.println("\n");
 		
 		//4. Print all the Strings using a for-each loop
+		for(String w : weekDays) {
+			System.out.println(w);
+		
+		}
+		
+		System.out.println("\n");
 		
 		//5. Print only the even numbered elements in the list.
+		System.out.println(weekDays.get(0));
+		System.out.println(weekDays.get(2));
+		System.out.println(weekDays.get(4));
 		
 		//6. Print all the Strings in reverse order.
+		System.out.println("\n");
+		System.out.println(weekDays.get(4));
+		System.out.println(weekDays.get(3));
+		System.out.println(weekDays.get(2));
+		System.out.println(weekDays.get(1));
+		System.out.println(weekDays.get(0));
 		
 		//7. Print only the Strings that have the letter 'e' in them.
+		System.out.println("\n");
+		System.out.println(weekDays.get(1));
+		System.out.println(weekDays.get(2));
 	}
 }
